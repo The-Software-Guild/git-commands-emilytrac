26fddab4487b54fd1e6fe9e4b9a5bfc58b7ba5fd
Emily Tracey <emilytracey21@gmail.com>
2022-10-11 16:18:54 +0100

    Hello Name code created

---
+++ HelloName.java
@@ -0,0 +1,11 @@
+/*
+ * @author Emily
+ * date: 11/10/2022
+ * purpose: Saying hello to someone
+ */
+
+public class HelloName {
+    public static void main(String[] args) {
+        System.out.println("Hello, Emily!");
+    }
+}
