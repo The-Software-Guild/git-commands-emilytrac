c873b26f1975b5e55c3b28efb473f65561f8d2bd
Emily Tracey <emilytracey21@gmail.com>
2022-10-11 15:59:12 +0100

    New Hello World

---
+++ HelloWorld/src/main/java/com/et/helloworld/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * @author emilytracey
+ * *email: emilytracey21@gmail.com
+ * date: 11/10/2022
+ * purpose: NetBeans Hello World!!
+ */
+
+package com.et.helloworld;
+
+public class Hello {
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Emily!");
+    }
+}
